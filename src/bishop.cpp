de574be5dcae8463382b1474714a227c36fc70ba
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-03-04 11:41:48 -0500

    bishop movement finished

--- src/bishop.cpp
+++ src/bishop.cpp
@@ -66,5 +66,53 @@ std::string Bishop::getTexturePath()
 
 
 bool Bishop::isMoveValid(int x1, int y1, int x2, int y2, std::string **board){
-    return true;
+    //if same color piece is in the way
+    if(board[y2][x2] != "  "){
+        if(isWhite){
+            if(board[y2][x2][1] == 'W')
+                return false;
+        }else{
+            if(board[y2][x2][1] == 'B')
+                return false;
+        }
+    }
+    if(x1 == x2){
+        if(y1 < y2){
+            for(int i = y1 + 1; i < y2; i++){
+                if(board[i][x1] != "  ")
+                    return false;
+            }
+        }else{
+            for(int i = y1 - 1; i > y2; i--){
+                if(board[i][x1] != "  ")
+                    return false;
+            }
+        }
+        return false;
+    }
+    if(abs(x1 - x2) == abs(y1 - y2)){
+        if(x1 < x2 && y1 < y2){
+            for(int i = 1; i < abs(x1 - x2); i++){
+                if(board[y1 + i][x1 + i] != "  ")
+                    return false;
+            }
+        }else if(x1 > x2 && y1 > y2){
+            for(int i = 1; i < abs(x1 - x2); i++){
+                if(board[y1 - i][x1 - i] != "  ")
+                    return false;
+            }
+        }else if(x1 < x2 && y1 > y2){
+            for(int i = 1; i < abs(x1 - x2); i++){
+                if(board[y1 - i][x1 + i] != "  ")
+                    return false;
+            }
+        }else if(x1 > x2 && y1 < y2){
+            for(int i = 1; i < abs(x1 - x2); i++){
+                if(board[y1 + i][x1 - i] != "  ")
+                    return false;
+            }
+        }
+        return true;
+    }
+    return false;
 }
